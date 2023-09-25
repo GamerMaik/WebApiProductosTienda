ea875dc9457f57a8babc389ae16170b2d5295eb6
ElTioCrack <felipezilian7@gmail.com>
2023-09-25 16:15:17 -0400

    LN

--- LN/ProductService.cs
+++ LN/ProductService.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ENTIDADES;
+using System.Runtime.InteropServices;
+using System;
+using DAL;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LN
 {
-    internal class ProductService
+    public class PersonController
     {
+        public ProductRepository _productRepository;
+
+        public PersonController()
+        {
+            _productRepository = new ProductRepository();
+        }
+
+        public void GetProduct(int id)
+        {
+            //var person = _productRepository.GetPersonById(id);
+        }
+
+        public void AddPerson(EProduct product)
+        {
+            try
+            {
+                //_productRepository.Adicionar_eEProduct(eEProduct);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+
+        public bool UpdatePerson()
+        {
+            //return _productRepository.UpdatePerson();
+            return true;
+        }
     }
 }
