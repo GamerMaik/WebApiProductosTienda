d7fe6aa146c6f565e054e760a1d387632638a7f6
ElTioCrack <felipezilian7@gmail.com>
2023-09-25 15:59:12 -0400

    funciones a ProdctController

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ENTIDADES;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Api.Controllers
 {
-    public class ProductController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet("{id}")]
+        public ActionResult<EProduct> GetPerson(int id)
         {
-            return View();
+            // var product = _personService.GetPersonById(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<EProduct> AddPerson([FromBody] EProduct product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePerson(int id, [FromBody] EProduct product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            bool updated = true;
+            //var updated = _personService.UpdatePerson(person);
+
+            if (!updated)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePerson(int id)
+        {
+            bool deleted = true;
+            //var deleted = _personService.DeletePerson(id);
+
+            if (!deleted)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
+
 }
