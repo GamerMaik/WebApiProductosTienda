f3ed642e7870a7322f85b9361e6adacc7a9ba1e4
Royer Ticona <royergt912@gmail.com>
2023-09-25 17:16:45 -0400

    Optimizando Codigo

--- DAL/SWLNPersona.cs
+++ DAL/SWLNPersona.cs
@@ -6,91 +6,108 @@ namespace DAL
 {
     public class SWLNPersona
     {
-        // Método para agregar un producto a la base de datos.
+        // Método para agregar un nuevo producto a la base de datos
         public void Adicionar_EProducto(EProduct eProduct)
         {
             try
             {
+                // Crear un contexto de base de datos
                 using (var context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>()))
                 {
+                    // Agregar el nuevo producto al conjunto de entidades
                     context.Product.Add(eProduct);
+
+                    // Guardar los cambios en la base de datos
                     context.SaveChanges();
                 }
             }
+
             catch (Exception ex)
             {
+                // Manejar excepciones
                 Console.WriteLine("Error al guardar cambios en la base de datos");
                 Console.WriteLine(ex.ToString());
                 throw;
             }
         }
 
-        // Método para eliminar un producto por su ID.
+        // Método para eliminar un producto de la base de datos por su ID
         public void Eliminar_Producto(int productId)
         {
             try
             {
+                // Crear un contexto de base de datos
                 using (var context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>()))
                 {
+                    // Buscar el producto por su ID
                     var productToDelete = context.Product.Find(productId);
 
                     if (productToDelete != null)
                     {
+                        // Si se encuentra el producto, eliminarlo de la base de datos
                         context.Product.Remove(productToDelete);
+
+                        // Guardar los cambios en la base de datos
                         context.SaveChanges();
                         Console.WriteLine("Producto eliminado correctamente.");
                     }
                     else
                     {
+                        // Si no se encuentra el producto, mostrar un mensaje
                         Console.WriteLine("No se encontró el producto con el ID especificado.");
                     }
                 }
             }
+
             catch (Exception ex)
             {
+                // Manejar excepciones
                 Console.WriteLine("Error al eliminar el producto de la base de datos.");
                 Console.WriteLine(ex.ToString());
                 throw;
             }
         }
 
-        // Método para actualizar un producto por su ID con nuevos datos.
+        // Método para actualizar un producto en la base de datos por su ID
         public void Actualizar_Producto(int productId, EProduct newProductData)
         {
             try
             {
-                // Crea un contexto de base de datos utilizando Entity Framework Core.
+                // Crear un contexto de base de datos
                 using (var context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>()))
                 {
-                    // Busca el producto en la base de datos por su ID.
+                    // Buscar el producto por su ID
                     var productToUpdate = context.Product.Find(productId);
 
                     if (productToUpdate != null)
                     {
-                        // Actualiza los campos del producto con los nuevos datos proporcionados.
+                        // Si se encuentra el producto, actualizar sus campos con los nuevos datos
                         productToUpdate.Name = newProductData.Name;
                         productToUpdate.Description = newProductData.Description;
                         productToUpdate.Price = newProductData.Price;
                         productToUpdate.StockQuantity = newProductData.StockQuantity;
-                        // Puedes agregar más campos aquí según tu modelo de datos.
+                        // Agregar otras actualizaciones aquí según tu modelo de datos
 
-                        // Guarda los cambios en la base de datos.
+                        // Guardar los cambios en la base de datos
                         context.SaveChanges();
                         Console.WriteLine("Producto actualizado correctamente.");
                     }
                     else
                     {
+                        // Si no se encuentra el producto, mostrar un mensaje
                         Console.WriteLine("No se encontró el producto con el ID especificado.");
                     }
                 }
             }
+
             catch (Exception ex)
             {
-                // Captura y maneja cualquier excepción que pueda ocurrir durante la actualización.
+                // Manejar excepciones
                 Console.WriteLine("Error al actualizar el producto en la base de datos.");
                 Console.WriteLine(ex.ToString());
-                throw; // Relanza la excepción para su manejo por código externo si es necesario.
+                throw;
             }
         }
+
     }
 }
